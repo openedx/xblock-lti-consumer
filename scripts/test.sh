896979c5e70e8f5a1a3650025e24780d1aa6fc4d
Qubad786 <muhammadrehan69@gmail.com>
2017-05-04 20:27:49 +0500

    convert to django app

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-export DJANGO_SETTINGS_MODULE="workbench.settings"
 mkdir -p var
 rm -rf .coverage
-nosetests --with-coverage --cover-package="lti_consumer"
+python test.py $1 --noinput
