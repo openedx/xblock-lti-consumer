fc35e2494632f1a9ced6a830e57a9ecd5696081d
Farhanah Sheets <fsheets@edx.org>
2018-02-08 10:29:54 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def package_data(pkg, roots):
 
 setup(
     name='lti_consumer-xblock',
-    version='1.1.6',
+    version='1.1.7',
     description='This XBlock implements the consumer side of the LTI specification.',
     packages=[
         'lti_consumer',
