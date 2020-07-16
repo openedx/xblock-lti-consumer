85b3a7ae68401e98d3b65bfc0f829631839bbad5
Ahmad Bilal Khalid <ahmedbilal96@gmail.com>
2020-07-17 00:31:01 +0500

    Make Lti Consumer indexable

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ with open('README.rst') as _f:
 
 setup(
     name='lti-consumer-xblock',
-    version='2.0.3',
+    version='2.1.0',
     description='This XBlock implements the consumer side of the LTI specification.',
     long_description=long_description,
     long_description_content_type='text/markdown',
