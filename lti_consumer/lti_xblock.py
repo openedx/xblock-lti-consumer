1b402eea0bfd51b05dc1630b0d7d910800b437bf
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2021-04-21 11:46:27 -0300

    fix: Add missing lti 1.3 variable to `hide_fields`

--- lti_consumer/lti_xblock.py
+++ lti_consumer/lti_xblock.py
@@ -622,6 +622,7 @@ class LtiConsumerXBlock(StudioEditableXBlockMixin, XBlock):
                 'lti_1p3_launch_url',
                 'lti_1p3_oidc_url',
                 'lti_1p3_tool_public_key',
+                'lti_advantage_ags_mode',
                 'lti_advantage_deep_linking_enabled',
                 'lti_advantage_deep_linking_launch_url',
             ]
