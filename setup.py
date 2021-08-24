e8d4e6af996d24f4b03e78d74b8a09ad094b043e
Soban Javed <iamsobanjaved@gmail.com>
2021-08-24 18:58:08 +0500

    chore: version bump for release

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ with open('README.rst') as _f:
 
 setup(
     name='lti-consumer-xblock',
-    version='3.0.3',
+    version='3.0.4',
     author='Open edX project',
     author_email='oscm@edx.org',
     description='This XBlock implements the consumer side of the LTI specification.',
