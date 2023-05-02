1f88dc31ecbf3e17b42c7320f117789cfbdf75cb
Zach Hancock <zhancock@edx.org>
2023-05-02 09:50:23 -0400

    feat: support assessment control claims

--- lti_consumer/lti_1p3/consumer.py
+++ lti_consumer/lti_1p3/consumer.py
@@ -873,6 +873,9 @@ class LtiProctoringConsumer(LtiConsumer1p3):
 
         self.set_extra_claim(proctoring_claims)
 
+        if self.proctoring_data.get("assessment_control_url"):
+            self.set_extra_claim(self.get_assessment_control_claims())
+
         return super().generate_launch_request(preflight_response)
 
     def check_and_decode_token(self, token):
